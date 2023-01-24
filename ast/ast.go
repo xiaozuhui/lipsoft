d21f314e40a8ea7c0ea0123860c10529ad594f7c
xuzh <1143841809@qq.com>
2023-01-24 20:31:39 +0800

    feat: 解析let语句

--- ast/ast.go
+++ ast/ast.go
@@ -1,14 +1,17 @@
 package ast
 
+import "lipsoft/token"
+
 /*
  * @Author: xiaozuhui
  * @Date: 2023-01-18 17:00:18
  * @LastEditors: xiaozuhui
  * @LastEditTime: 2023-01-18 17:03:56
- * @Description:
+ * @Description: let x = 5;
  */
 
 type Node interface {
+	// TokenLiteral 返回关联的词法单元的字面量
 	TokenLiteral() string
 }
 
@@ -22,6 +25,7 @@ type Expression interface {
 	expressionNode()
 }
 
+// Program 每个ast节点的根节点
 type Program struct {
 	Statements []Statement
 }
@@ -32,4 +36,29 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+// LetStatement 标识符、产生值的表达式、词法单元
+type LetStatement struct {
+	Token token.Token // token.LET词法单元
+	Name  *Identifier // 绑定的标识符
+	Value Expression  // 产生值的表达式
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier 即`let x = 5;`中的x
+type Identifier struct {
+	Token token.Token // token.IDENT词法单元
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
---
+++ parser/parser.go
@@ -0,0 +1,105 @@
+package parser
+
+import (
+	"fmt"
+	"lipsoft/ast"
+	"lipsoft/lexer"
+	"lipsoft/token"
+)
+
+type Parser struct {
+	lexer *lexer.Lexer // 词法分析器实例
+
+	curToken  token.Token // 当前词法单元
+	peekToken token.Token // 下一个词法单元
+
+	errors []string // 错误处理
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := Parser{lexer: l, errors: []string{}}
+
+	p.nextToken() // 读取两个词法单元
+	p.nextToken()
+
+	return &p
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{} // 构造ast根节点
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) { // 遍历输入的词法单元
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken() // 同时前移curToken和peekToken
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// 如果下一个此法单元不是标识符，比如变量，或是字符串字面量，则结束
+	if !p.exceptPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// 如果下一个词法单元不是=
+	if !p.exceptPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO 跳过对表达式的操作，直到遇到分号;
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return t == p.curToken.Type
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return t == p.peekToken.Type
+}
+
+// exceptPeek 判断下一个token是否是期望词法单元，如果是，则后移词法单元指针
+func (p *Parser) exceptPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	p.peekError(t) // 错误
+	return false
+}
